b1355b4f3fb6170eda41d9455423433c5624262b
Euan Gilmour <euan.gilmour.2021@uni.strath.ac.uk>
2024-01-31 11:42:21 +0000

    Add TCPClient code from previous lab

---
+++ cs313_main/src/TCPClient.java
@@ -0,0 +1,31 @@
+
+import java.net.*;
+import java.io.*;
+
+public class TCPClient {
+
+    public static void main(String[] args) {
+
+        String hostName = args[0];
+        int portNumber = Integer.parseInt(args[1]);
+
+        try (Socket clientSocket = new Socket(hostName, portNumber)) {
+
+            PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
+            BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+            BufferedReader stdIn = new BufferedReader(new InputStreamReader(System.in));
+
+            System.out.println("Input a message to send to the server.");
+
+            String message;
+            while ((message = stdIn.readLine()) != null) {
+                out.println(message);
+                System.out.println("Response: '" + in.readLine() + "'");
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+    }
+
+}
