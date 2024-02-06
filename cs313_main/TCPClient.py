1ded56c0ffb159b121e0dbb99641cf30a3a39713
Euan Gilmour <euan.gilmour.2021@uni.strath.ac.uk>
2024-02-06 20:43:29 +0000

    Implement multithreaded message sending/receiving for Python client
    Our Python client is now multithreaded, and can speak to our multithreaded Java TCP Server

--- cs313_main/TCPClient.py
+++ cs313_main/TCPClient.py
@@ -1,11 +1,40 @@
 from socket import *
-serverName = 'servername'
-serverPort = 12000
+import threading
+import sys
+
+
+def inputHandler(socket):
+    while True:
+        buffer = b""
+        byte = b""
+        while byte != b"\n":
+            byte = socket.recv(1)
+            if not byte:
+                break
+            buffer += byte
+        print("\033[K", end="")
+        print("\rchum> " + buffer.decode("utf-8"), end="")
+        print("\033[Kyou> ", end="", flush=True)
+
+
+def outputHandler(socket):
+    while True:
+        message = input("you> ")
+        message += "\n"
+        socket.send(message.encode("utf-8"))
+
+
+serverName = sys.argv[1]
+serverPort = int(sys.argv[2])
 
 clientSocket = socket(AF_INET, SOCK_STREAM)
 clientSocket.connect((serverName, serverPort))
-sentence = input('Input lowercase sentence:')
-clientSocket.send(sentence.encode())
-modifiedSentence = clientSocket.recv(1024)
-print('From server: ', modifiedSentence.decode())
-clientSocket.close()
+
+inputThread = threading.Thread(target=inputHandler, args=(clientSocket,))
+outputThread = threading.Thread(target=outputHandler, args=(clientSocket,))
+
+inputThread.start()
+outputThread.start()
+
+while inputThread.is_alive() and outputThread.is_alive():
+    pass
