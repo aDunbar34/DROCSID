15c92c377c73faef637694c7abaf56250f591d5b
Euan Gilmour <euan.gilmour.2021@uni.strath.ac.uk>
2024-02-13 21:53:40 +0000

    Set up branch for stage 2 implementation

--- cs313_main/src/Client.java
+++ cs313_main/src/Client.java
@@ -1,42 +1,46 @@
 import java.net.*;
-import java.io.*;
 
-/// The client class makes a connection to the server
-/// and allows for sending and receiving messages
 public class Client {
 
     public static void main(String[] args) {
 
-        // Get hostname and port number of the server form the command line
-        String hostName = args[0];
-        int portNumber = Integer.parseInt(args[1]);
+        System.out.println("""
+                ██████╗ ██████╗  ██████╗  ██████╗███████╗██╗██████╗
+                ██╔══██╗██╔══██╗██╔═══██╗██╔════╝██╔════╝██║██╔══██╗
+                ██║  ██║██████╔╝██║   ██║██║     ███████╗██║██║  ██║
+                ██║  ██║██╔══██╗██║   ██║██║     ╚════██║██║██║  ██║
+                ██████╔╝██║  ██║╚██████╔╝╚██████╗███████║██║██████╔╝
+                ╚═════╝ ╚═╝  ╚═╝ ╚═════╝  ╚═════╝╚══════╝╚═╝╚═════╝
+                    """);
+        System.out.println("You are now experiencing DROCSID");
 
-        // Try to establish a connection to the server
-        try (Socket clientSocket = new Socket(hostName, portNumber)) {
 
-            System.out.println("Connection established.");
+        String hostname = args[0];
+        int portNo = Integer.parseInt(args[1]);
 
-            // Set up writers and readers for UTF-8
-            PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
-            BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
-            BufferedReader stdIn = new BufferedReader(new InputStreamReader(System.in));
-
-            // The basic loop - Get a message from stdin,
-            // wait for a response from the server,
-            // print response, repeat
-            while (true) {
-                System.out.print("you> ");
-
-                String message;
-                message = stdIn.readLine();
-                    out.println(message);
-                    String response = in.readLine();
-                    System.out.println("buddy> " + response);
-            }
+        System.out.println("Attempting to connect to client '" + hostname + "' on port " + portNo + "...");
+        try (Socket socket = new Socket(hostname, portNo)) {
+            System.out.println("Connection established! Enjoy your chat!");
+            chatLoop(socket);
         } catch (Exception e) {
             e.printStackTrace();
         }
 
+    }
+
+    // Sets up threads and runs the primary chat loop
+    public static void chatLoop(Socket socket) {
+        PeerInput peerInput = new PeerInput(socket);
+        PeerOutput peerOutput = new PeerOutput(socket);
+
+        Thread inputThread = new Thread(peerInput);
+        Thread outputThread = new Thread(peerOutput);
+
+        inputThread.start();
+        outputThread.start();
+
+        while (inputThread.isAlive() && outputThread.isAlive()) {
+        }
     }
 
 }
