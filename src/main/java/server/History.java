ebe7f2aacf38850874f553870cb629f8a541e064
Robbie Booth CS2021 <robbie.booth.2021@uni.strath.ac.uk>
2024-03-05 21:08:27 +0000

    added creating user

--- src/main/java/server/History.java
+++ src/main/java/server/History.java
@@ -2,11 +2,23 @@ package server;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.RandomAccessFile;
+import java.nio.channels.FileChannel;
+import java.nio.channels.FileLock;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.List;
 
 public class History {
 
-    private ObjectMapper objectMapper = new ObjectMapper();
+    private static final ObjectMapper objectMapper = new ObjectMapper();
+
+    private static final Path roomsPathDir = Paths.get("rooms");
+    private static final Path usersPathDir = Paths.get("users");
 
 
     /**
@@ -54,12 +66,56 @@ public class History {
 
     /**
      * Creates a user file of the username provided. If file exists nothing happens.
-     * @param username Name of user to create
+     * @param username Name of user to create. If empty no file is created.
      *
      * @author Robbie Booth
      */
-    public void createUser(String username){
+    public static void createUser(String username) throws IOException {
+        //create user path directory if it does not exist
+        History.checkPathElseCreate(usersPathDir);
 
+        File file = new File(usersPathDir.toFile(), username+".json");
+        if(file.exists()){
+            return;
+        }
+        //username invalid
+        if(username == null || username.isBlank()){
+            return;
+        }
+        RandomAccessFile fileWriter = null;
+        FileChannel channel = null;
+        FileLock lock = null;
+        String jsonContent = "{ \"username\": \""+username+"\", \"rooms\": [] }";
+        try {
+
+            // Open the file in read-write mode
+            fileWriter = new RandomAccessFile(file, "rw");
+            channel = fileWriter.getChannel();
+
+            // Acquire an exclusive lock on the file
+            lock = channel.lock();
+
+            // Set the file pointer to the beginning of the file to overwrite its content
+            fileWriter.seek(0);
+
+            // Truncate the file to remove any existing content
+            fileWriter.setLength(0);
+
+            fileWriter.writeBytes(jsonContent);
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }finally {
+            if(lock != null){
+                lock.release();
+            }
+            if(channel != null){
+                channel.close();
+            }
+            if(fileWriter != null){
+                fileWriter.close();
+            }
+        }
     }
 
     /**
@@ -76,5 +132,22 @@ public class History {
 
     public void addMessageToHistory(){
 
+    }
+
+    /**
+     * If path doesn't exist then it creates it.
+     * @param path path to check if exists/create
+     * @throws IOException
+     * @author Robbie Booth
+     */
+    public static void checkPathElseCreate(Path path) throws IOException {
+        if(!Files.exists(path)){
+            Files.createDirectories(path);
+        }
+    }
+
+
+    public static void main(String[] args) throws IOException {
+        History.createUser("robert");
     }
 }
