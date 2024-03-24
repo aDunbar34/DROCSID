f9a7197a5d7449d239ca2720ef62692f056c2a47
Max <max.bottcher-allison.2021@uni.strath.ac.uk>
2024-03-24 23:12:23 +0000

    started idea for online test

--- src/main/java/client/ClientProducer.java
+++ src/main/java/client/ClientProducer.java
@@ -106,7 +106,8 @@ public class ClientProducer implements Runnable {
                 case "\\sendRequest" -> handleSendRequests(commandArgs);
                 case "\\accept" -> handleAcceptRequest(commandArgs);
                 case "\\startHistoryTest" -> startHistoryTest(commandArgs);
-                case "\\startOnlineTest" -> startOnlineTest(commandArgs);
+                case "\\startOnlineTest1" -> startOnlineTestJoiningLeaving();
+                case "\\startOnlineTest2" -> startOnlineTestReqOnline();
                 default -> System.out.println("Unrecognized command: '" + commandArgs[0] + "'.");
             }
         } else { // Treat input as message
@@ -655,42 +656,43 @@ public class ClientProducer implements Runnable {
         }
     }
 
-    private void startOnlineTest(String args[]) {
-        String roomName = "onlineTest";
-        System.out.println(args[1]);
+    private void startOnlineTestJoiningLeaving() {
 
-        // arguments validation
-        if (!args[1].equals("even") && !args[1].equals("odd")) {
-            System.out.println("Invalid argument. Usage: \\startHistoryTest [even|odd]");
-            return;
-        }
+        String roomName = "onlineTest";
 
         // start a thread for leaving & joining room every odd millisecond
-        if (args[1].equals("odd")) {
-            Thread leaveJoinThread = new Thread(() -> {
-                while (true) {
-                    try {
-                        exitRoom();
-                        Thread.sleep(1); // wait 1 millisecond
-                        joinRoom(new String[]{"join", roomName});
-                        Thread.sleep(1); // wait 1 millisecond
-                    } catch (InterruptedException e) {
-                        throw new RuntimeException(e);
-                    }
+        while (true) {
+
+            long time = System.currentTimeMillis();
+            if(time % 2 == 0) {
+                try {
+                    Thread.sleep(5000);
+                    joinRoom(new String[]{"join", roomName});
+                    Thread.sleep(1); // wait 1 millisecond
+                    exitRoom();
+                    Thread.sleep(1); // wait 1 millisecond
                 }
-            });
-            leaveJoinThread.start();
+                catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+            }
         }
+    }
+
+    private void startOnlineTestReqOnline() {
 
         // request online status every even millisecond
         while (true) {
-            try {
-                Thread.sleep(1); // wait 1 millisecond
-                if (args[1].equals("even")) {
+            long time = System.currentTimeMillis();
+            if (time % 2 != 0) {
+                try {
+                    Thread.sleep(5000);
                     showOnline();
+                    Thread.sleep(1); // wait 1 millisecond
                 }
-            } catch (InterruptedException e) {
-                throw new RuntimeException(e);
+                catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
             }
         }
     }
